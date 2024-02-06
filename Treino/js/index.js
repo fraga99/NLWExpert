2092b5cbd97e92c11bafd5fb67515f657e896b08
Fraga <diogo_avila99@hotmail.com>
2024-02-06 19:54:19 -0300

    modified:   Treino/js/index.js

--- Treino/js/index.js
+++ Treino/js/index.js
@@ -1,3 +1,14 @@
 const perguntas = [
-
+    {
+        pergunta: "Pergunta 01: ",
+        respostas: [
+            "Resposta A",
+            "Resposta B",
+            "Resposta C",
+        ],
+        correta: 2
+    }, 
 ]
+
+alert (perguntas[0].pergunta)
+
