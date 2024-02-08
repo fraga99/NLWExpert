13f6aeef2680386b7d6a8ebf68300188008b63d1
Fraga <diogo_avila99@hotmail.com>
2024-02-08 09:24:55 -0300

    att

--- Treino/js/index.js
+++ Treino/js/index.js
@@ -94,9 +94,9 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
-
 for(const item of perguntas)   {
     const quizItem = template.content.cloneNode(true)
+    quiz.appendChild(quizItem)
     quizItem.querySelector('h3').textContent = item.pergunta
     
     for(let resposta of item.respostas)   {
@@ -106,8 +106,8 @@ for(const item of perguntas)   {
         quizItem.querySelector('dl').appendChild(dt)
     }
 
-    quizItem.querySelector('dl dt').remove()
-    //coloca a pergunta na tela
-    quiz.appendChild(quizItem)
-
+    
 }
+quizItem.querySelector('dl dt').remove()
+//coloca a pergunta na tela
+quiz.appendChild(quizItem)
